876628cd307c2c98541c5a56ce13b8a0aacb136a
Spokojnie1 <alterna1@o2.pl>
2016-08-23 20:46:34 +0200

    szkolenie 2
    LinearLayout

---
+++ app/src/main/java/com/example/kot/myapplication/Domowe.java
@@ -0,0 +1,13 @@
+package com.example.kot.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Domowe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_domowe);
+    }
+}
